118276dedce0ae7506cdd72a8ec8ff4b95274a38
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-09-09 02:09:09 +0000

    Revert "[Finish] 応用課題6"
    This reverts commit ca3e8060c65d24abb8f6eca9ca6447df062ec10f.

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,29 +1,24 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    @book_comments = BookComment.where(book_id: @book.id)
+    @book_comment.book_id = book.id
     if @book_comment.save
-       @book_comment = BookComment.new
-      #redirect_to book_path(book)
+      redirect_to book_path(book)
       #redirect_back fallback_location: root_path
     else
       @book = Book.find(params[:book_id])
       @new_book = Book.new
       @user = @book.user
       @book_comments = BookComment.where(book_id: @book.id)
-      #render "books/show"
+      render "books/show"
     end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @book_comments = BookComment.where(book_id: @book.id)
     book_comment = BookComment.find(params[:id])
     book_comment.destroy
-    @book_comment = BookComment.new
-    #redirect_back fallback_location: root_path
+    redirect_back fallback_location: root_path
   end
   
   private
